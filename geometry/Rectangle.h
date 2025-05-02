005b179780cf52a5aaa276603d26d2f9b03483b3
AleXutzZu <davidalexrobertyt@gmail.com>
2025-05-03 00:49:46 +0300

    Added documentation for rectangle class

--- geometry/Rectangle.h
+++ geometry/Rectangle.h
@@ -8,7 +8,9 @@
 #include "Point.h"
 
 namespace imgproc {
-
+    /**
+     * Class to represent a basic Rectangle shape
+     */
     class Rectangle {
     private:
         unsigned int x, y;
@@ -16,14 +18,40 @@ namespace imgproc {
         unsigned int height;
 
     public:
+        /**
+         * Constructs a rectangle with all values set to 0
+         */
         Rectangle();
-
+        /**
+         * Constructs a rectangle given the top-left corner, a width and a height
+         * @param x the x-coordinate for the top-left corner
+         * @param y the y-coordinate for the top-left corner
+         * @param width the width of the rectangle
+         * @param height the height of the rectangle
+         */
         Rectangle(unsigned int x, unsigned int y, unsigned int width, unsigned int height);
-
+        /**
+         * Constructs a rectangle given the top-left and bottom-right corners
+         * @param topLeft the top-left corner of the rectangle
+         * @param bottomRight the bottom-right corner of the rectangle
+         * @throws runtime_error if the specified coordinates would produce a rectangle with negative width/height
+         */
         Rectangle(const Point &topLeft, const Point &bottomRight);
 
+        /**
+         * Performs the union of two rectangles
+         * @param lhs the first rectangle
+         * @param rhs the other rectangle
+         * @return the smallest rectangle which contains both @a lhs and @a rhs
+         */
         friend Rectangle operator|(const Rectangle &lhs, const Rectangle &rhs);
 
+        /***
+         * Performs he intersection of two rectangles
+         * @param lhs the first rectangle
+         * @param rhs the second rectangle
+         * @return the rectangle at the intersection of @a lhs with @a rhs
+         */
         friend Rectangle operator&(const Rectangle &lhs, const Rectangle &rhs);
 
         friend std::istream &operator>>(std::istream &is, Rectangle &rectangle);
