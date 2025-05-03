2921dc4499fbb392682c47c622ccfe335470193c
AleXutzZu <davidalexrobertyt@gmail.com>
2025-05-03 22:56:03 +0300

    Added documentation for the ImageProcessing interface

--- image/processors/ImageProcessing.h
+++ image/processors/ImageProcessing.h
@@ -8,9 +8,18 @@
 #include "../Image.h"
 
 namespace imgproc {
+    /**
+     * Base interface for applying an operation on an image
+     * @tparam T the type of image this processor applies to
+     */
     template<typename T>
     class ImageProcessing {
     public:
+        /**
+         * Processes the image stored in @a src and stores the result in @a dst
+         * @param src the image to process
+         * @param dst the output image
+         */
         virtual void processImage(const T &src, T &dst) = 0;
 
         virtual ~ImageProcessing() = default;
