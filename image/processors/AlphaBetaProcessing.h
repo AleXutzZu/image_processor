ffcb843177f9661c7099d968b5ddc6f92460ec24
AleXutzZu <davidalexrobertyt@gmail.com>
2025-05-03 23:02:03 +0300

    Added documentation to the alpha-beta processor

--- image/processors/AlphaBetaProcessing.h
+++ image/processors/AlphaBetaProcessing.h
@@ -9,18 +9,38 @@
 #include "../PPMImage.h"
 
 namespace imgproc {
-
+    /**
+     * Processor responsible for applying alpha-beta corrections on the PPM and PGM image formats
+     */
     class AlphaBetaProcessing : public PGMImageProcessor, public PPMImageProcessor {
     private:
         float alpha, beta;
 
     public:
+        /**
+         * Constructs an alpha-beta processor with the specified values
+         * @param alpha the alpha value
+         * @param beta the beta value
+         */
         AlphaBetaProcessing(float alpha, float beta);
 
+        /**
+         * Constructs a default no-op processor with alpha value of 1 and beta value of 0
+         */
         AlphaBetaProcessing();
 
+        /**
+         * Applies alpha-beta correction to PGM images
+         * @param src the source image
+         * @param dst the modified image
+         */
         void processImage(const PGMImage &src, PGMImage &dst) override;
 
+        /**
+         * Applies alpha-beta correction to PPM images
+         * @param src the source image
+         * @param dst the modified image
+         */
         void processImage(const PPMImage &src, PPMImage &dst) override;
     };
 
