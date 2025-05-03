f83fcfa25f246920f66c713d1fda220115f3f8b0
AleXutzZu <davidalexrobertyt@gmail.com>
2025-05-03 23:13:48 +0300

    Added documentation to the base kernel and the identity kernel classes

--- image/processors/kernels/IdentityKernel.h
+++ image/processors/kernels/IdentityKernel.h
@@ -9,7 +9,10 @@
 #include "../../pixel/pixel.h"
 
 namespace imgproc {
-
+    /**
+     * No-op kernel which does not alter the image. The scaling function returns the value itself after applying this kernel,
+     * which in this case would be the original pixel value
+     */
     class IdentityKernel : public Kernel {
     public:
         IdentityKernel();
--- image/processors/kernels/Kernel.h
+++ image/processors/kernels/Kernel.h
@@ -9,7 +9,9 @@
 #include "../../pixel/pixel.h"
 
 namespace imgproc {
-
+    /**
+     * Abstract class representing a kernel used for convolution operations
+     */
     class Kernel {
     protected:
         unsigned int width{};
@@ -20,17 +22,41 @@ namespace imgproc {
         [[nodiscard]] bool _valid(int i, int j) const;
 
     public:
-
+        /**
+         * Constructs a matrix filled with the value 0 with the specified width and height and a scaling function
+         * @param width the width of the matrix
+         * @param height the height of the matrix
+         * @param function the scale function applied to the result
+         */
         Kernel(unsigned int width, unsigned int height, const std::function<GrayPixel(int)> &function);
 
         virtual ~Kernel();
 
+        /**
+         * Returns the value at position (i, j) in the matrix
+         * @param i the row
+         * @param j the column
+         * @return the value at row i and column j in the matrix
+         * @throws runtime_error if the matrix is empty (as in a nullptr) or the coordinates are invalid
+         */
         [[nodiscard]] const int &at(int i, int j) const;
 
+        /**
+         * Returns the width of the matrix
+         * @return the width
+         */
         [[nodiscard]] unsigned int getWidth() const;
 
+        /**
+         * Returns the height of the matrix
+         * @return the height
+         */
         [[nodiscard]] unsigned int getHeight() const;
 
+        /**
+         * Returns the scaling function which should be used on the kernel after applying it on a set of pixels
+         * @return the scaling function
+         */
         [[nodiscard]] const std::function<GrayPixel(int)> &getScalingFunction() const;
     };
 
