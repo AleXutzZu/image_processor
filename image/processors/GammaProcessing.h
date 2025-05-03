cde42d76b40ab03569cd2e943f822c153e7b3c19
AleXutzZu <davidalexrobertyt@gmail.com>
2025-05-03 22:59:29 +0300

    Added documentation to the gamma processor

--- image/processors/GammaProcessing.h
+++ image/processors/GammaProcessing.h
@@ -9,18 +9,36 @@
 #include "../PPMImage.h"
 
 namespace imgproc {
-
+    /**
+     * Processor responsible for applying a gamma correction on the PPM and PGM image formats
+     */
     class GammaProcessing : public PPMImageProcessor, public PGMImageProcessor {
     private:
         float gamma;
 
     public:
+        /**
+         * Constructs a default no-op processor with a gamma value of 1
+         */
         GammaProcessing();
-
+        /**
+         * Constructs a gamma processor with the specified value
+         * @param gamma the gamma correction to apply on images
+         */
         explicit GammaProcessing(float gamma);
 
+        /**
+         * Applies gamma correction to PPM images
+         * @param src the source image
+         * @param dst the modified image
+         */
         void processImage(const PPMImage &src, PPMImage &dst) override;
 
+        /**
+         * Applies gamma correction to PGM images
+         * @param src the source image
+         * @param dst the modified image
+         */
         void processImage(const PGMImage &src, PGMImage &dst) override;
     };
 
