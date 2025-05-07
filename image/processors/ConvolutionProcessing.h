c1928b0235bc9719b0f33ad874989ee5d9eaa9a6
AleXutzZu <davidalexrobertyt@gmail.com>
2025-05-08 02:03:23 +0300

    Added documentation to the ConvolutionProcessing class

--- image/processors/ConvolutionProcessing.h
+++ image/processors/ConvolutionProcessing.h
@@ -11,18 +11,38 @@
 #include <functional>
 
 namespace imgproc {
-
+    /**
+     * Processor responsible for applying a kernel convolution on the PPM and PGM image formats
+     */
     class ConvolutionProcessing : public PGMImageProcessor, public PPMImageProcessor {
     private:
         Kernel *kernel;
 
     public:
+        /**
+         * Constructs a default no-op processor with the identity kernel
+         */
         ConvolutionProcessing();
 
+        /**
+         * Constructs a processor with the given kernel. This constructor takes full ownership of the deletion
+         * of the pointer to the kernel so it has to be heap allocated
+         * @param kernel a heap allocated kernel that will be managed the processor for de-allocation
+         */
         explicit ConvolutionProcessing(Kernel *kernel);
 
+        /**
+         * Applies kernel convolution to PPM images
+         * @param src the source image
+         * @param dst the modified image
+         */
         void processImage(const PPMImage &src, PPMImage &dst) override;
 
+        /**
+         * Applies kernel convolution to PGM images
+         * @param src the source image
+         * @param dst the modified image
+         */
         void processImage(const PGMImage &src, PGMImage &dst) override;
 
         ~ConvolutionProcessing() override;
