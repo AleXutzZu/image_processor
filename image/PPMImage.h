5551be4777f9153e1b081e34b1352ff191dcb903
AleXutzZu <davidalexrobertyt@gmail.com>
2025-05-07 21:50:29 +0300

    Added documentation to the PPMImage class

--- image/PPMImage.h
+++ image/PPMImage.h
@@ -21,8 +21,16 @@ namespace imgproc {
         static void kernelConvolution(const Kernel &kernel, PPMImage &vector);
 
     public:
+        /**
+         * Constructs a completely empty PPM image with no width, height or pixel data
+         */
         PPMImage();
 
+        /**
+         * Constructs a completely black PPM image with the given width and height
+         * @param width the width of the image
+         * @param height the height of the image
+         */
         PPMImage(unsigned int width, unsigned int height);
 
         PPMImage(const PPMImage &other);
@@ -31,12 +39,36 @@ namespace imgproc {
 
         void save(const std::string &imagePath) override;
 
+        /**
+         * Adds the scalar value to all the pixels in the image
+         * @param scalar the scalar to add
+         * @param vector the image to add the scalar to
+         * @return a new PPM image with the computed values for pixels
+         */
         friend PPMImage operator+(float scalar, const PPMImage &vector);
 
+        /**
+         * Multiplies all the pixels in the image by the scalar value
+         * @param scalar the scalar to multiply by
+         * @param vector the the image to have the scalar applied to
+         * @return a new PPM image with the computed values for pixels
+         */
         friend PPMImage operator*(float scalar, const PPMImage &vector);
 
+        /**
+         * Applies a convolution kernel to the image
+         * @param kernel the kernel to apply
+         * @param vector the image
+         * @return a new PPM image with the computed values for pixels
+         */
         friend PPMImage operator*(const Kernel &kernel, const PPMImage &vector);
 
+        /**
+         * Raises all pixels to the power of the scalar value
+         * @param scalar the scalar exponent
+         * @param vector the image
+         * @return a new PPM image with the computed values for pixels
+         */
         friend PPMImage operator^(float scalar, const PPMImage &vector);
 
         PPMImage &operator=(const PPMImage &other);
